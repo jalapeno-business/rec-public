341c89e1b1336d4108b26b75fa6c24bb423f5f61
Karin Hsu <khsu0720@gmail.com>
2018-09-18 23:23:19 -0700

    components built, eslint errors still present

--- client/src/components/InsiderTip.jsx
+++ client/src/components/InsiderTip.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import styles from './InsiderTip.styles.css';
 import container from './app.styles.css';
+import { join } from 'path';
 
 const InsiderTip = (props) => {
   const { insiderTip } = props;
   return (
-    <div>
+    <div className={styles.container}>
       <h1 className={container.headings}> Insider Tips </h1>
-      <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <div className={styles.tipDivider}></div>
         <div className={styles.text}>
-          {insiderTip}
+          {insiderTip} 
         </div>
+        <div className={styles.tipDivider}></div>
       </div>
     </div>
   );
